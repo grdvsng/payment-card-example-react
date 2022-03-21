0eaedd005df7b5ec423f1a1c349df065506b97b9
grdvsng <grdvsng@gmail.com>
2022-03-21 23:02:52 +0400

    fix

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,7 +3,7 @@ import express, { type Express } from 'express'
 import bodyParser                from 'body-parser';
 import dbInit                    from './db/init';
 
-export const PORT        = '8080';
+export const PORT        = '5000';
 export const ROOT_DIR    = path.dirname( path.dirname( __dirname ) );
 export const STATIC_ROOT = path.join( ROOT_DIR, 'client', 'build' );
 export const STATIC_DIRS = [ 
